c64397b48ae5b6dc9cebc749b73b96ced822db51
Tim Kotowski <tkotowski7@gmail.com>
2023-05-31 22:55:12 -0500

    added rust fmt action

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    if "sdsadasdasdasd'asd'asdl';lads';ldsa';ldas';ld'sa;ld';asdl';asdl'adsl';ldas';das';d" {
+        println!("Hello, world!");
+    }
 }
